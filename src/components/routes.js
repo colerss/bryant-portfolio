fd95bb9bfb3ae533acbdb0020ba8977e41bb7329
Bryant Suiskens <r0739290@student.thomasmore.be>
2023-06-06 19:30:04 +0200

    i18n fixed

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,23 +1,28 @@
-import { Routes, Route} from "react-router-dom";
-import { lazy } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { lazy, useEffect } from "react";
 const AboutMePage = lazy(() => import("../views/aboutme"));
 const InternshipPage = lazy(() => import("../views/internship"));
 const ProjectsPage = lazy(() => import("../views/projects"));
 const SkillsPage = lazy(() => import("../views/skills"));
 const JourneyPage = lazy(() => import("../views/journey"));
-const HomePage = lazy(()=> import("../views/home"));
+const HomePage = lazy(() => import("../views/home"));
 
-export default function AppRoutes(){
-        return (
-            <Routes>
-                <Route path="/" exact element={<HomePage />} />
-                <Route path="/bryant-portfolio/" exact element={<HomePage />} />
-                <Route path="/bryant-portfolio/aboutme" element={<AboutMePage />}  />
-                <Route path="/bryant-portfolio/journey" element={<JourneyPage />} />
-                <Route path="/bryant-portfolio/projects" element={<ProjectsPage />} />
-                <Route path="/bryant-portfolio/skills" element={<SkillsPage />}/>
-                <Route path="/bryant-portfolio/internship" element={<InternshipPage/>} />
-            </Routes>
-        )
+export default function AppRoutes() {
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      window.location.href = "/bryant-portfolio/home";
+    }
+  }, [location.pathname]);
 
+  return (
+    <Routes>
+      <Route path="/bryant-portfolio/home"  element={<HomePage />} />
+      <Route path="/bryant-portfolio/aboutme" element={<AboutMePage />} />
+      <Route path="/bryant-portfolio/journey" element={<JourneyPage />} />
+      <Route path="/bryant-portfolio/projects" element={<ProjectsPage />} />
+      <Route path="/bryant-portfolio/skills" element={<SkillsPage />} />
+      <Route path="/bryant-portfolio/internship" element={<InternshipPage />} />
+    </Routes>
+  );
 }
